fea3a288cff58f7d10d3eac53e340d0e17b579b5
ADOBENET\yerrola <yerrola@adobe.com>
2026-05-27 17:54:47 +0530

    fixing build errors

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -24,7 +24,7 @@ import {
 } from './util.js';
 import fetchStatesByCountry from './state-functions.js';
 
-export default { fetchStatesByCountry };
+export { fetchStatesByCountry };
 
 export const DELAY_MS = 0;
 let captchaField;
