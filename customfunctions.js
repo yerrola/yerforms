88eb26f71dff361551e54b8396f24de19dc961e3
ADOBENET\yerrola <yerrola@adobe.com>
2026-05-27 14:25:19 +0530

    Added a custom function

---
+++ customfunctions.js
@@ -0,0 +1,13 @@
+import { fetchStatesByCountry } from './functions/state-functions.js';
+
+export default {
+  fetchStatesByCountry: {
+    title: 'Fetch States by Country',
+    params: [
+      { name: 'countryCode', type: 'string' },
+      { name: 'globals',     type: 'object', required: false }
+    ],
+    returnType: 'void',
+    fn: fetchStatesByCountry
+  }
+};
